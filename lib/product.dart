cbbb3ba64a8c217c9ef96d03dfb948c6f7d1afca
@ayoubhofr <ayoubhofr@gmail.com>
2023-06-23 05:37:06 +0100

    first commit

---
+++ lib/product.dart
@@ -0,0 +1,12 @@
+class Product {
+  final int id, price;
+  final String title, subTitle, description, image;
+
+  Product(
+      {this.id,
+      this.price,
+      this.title,
+      this.subTitle,
+      this.description,
+      this.image}); 
+}
